a9944fa144a947add632009f3d3772d3d29ce69b
lokesh2506 <lokeshkumars2506@gmail.com>
2026-03-16 12:19:55 +0530

    Taskify App completed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.g49p9eh559g"
+    "revision": "0.pmbedpp0n7k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/EditTaskModal.jsx
+++ src/components/EditTaskModal.jsx
@@ -24,7 +24,7 @@ const EditTaskModal = ({ task, close }) => {
   };
   return (
     <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center">
-      <div className="bg-white p-6 rounded w-96">
+      <div className="bg-white p-6 rounded w-full max-w-96">
         <h2 className="text-xl font-bold mb-4">Edit Task</h2>
         <input
           className="border p-2 w-full mb-2"
